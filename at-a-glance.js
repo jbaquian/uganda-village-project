b0023f593ca0cbdfcf99830801924440c7cb4293
Jon Shilling <jon.shilling96@gmail.com>
2015-12-02 12:22:00 -0800

    -started visualization

---
+++ at-a-glance.js
@@ -0,0 +1,118 @@
+d3.json('gho-data.json', function(error, data) {
+	var labelVar = 'description'
+	var varNames = d3.keys(data[3])
+		.filter(function (key) {return key !== labelVar})
+
+	//color.domain(varNames)
+
+	var seriesData = varNames.map(function (name) {
+		return {
+			name: name,
+			values: data.map(function (d) {
+				return {name: name, label: d[labelVar], value: +d[name]}
+			})
+		}
+	})
+
+	console.log(seriesData)
+
+
+
+	var svg = d3.select('#vis')
+		.append('svg')
+		.attr('height', 400)
+		.attr('width', 400)
+
+		//margin
+		var margin = {
+			left: 70,
+			bottom: 100,
+			top:50,
+			right:50
+		}
+
+		//chart height and width
+		var height = 400 - margin.bottom - margin.top
+		var width = 400 - margin.left - margin.right
+
+		//positions the g element so it doesn't overlap the scales
+		var g = svg.append('g')
+			.attr('transform', 'translate(' +  margin.left + ',' + margin.top + ')')
+			.attr('height', height)
+			.attr('width', width)
+
+		//sets the scales
+		// var setScales = function(seriesData){
+			//xScale
+			var x = d3.scale.ordinal()
+				.rangeRoundBands([0, width], .1)
+
+			//yScale
+			// var min = d3.min(seriesData, function (c) { 
+		 //      return d3.min(c.values, function (d) { return d.value })
+		 //    })
+		 //    var max = d3.max(seriesData, function (c) { 
+		 //      return d3.max(c.values, function (d) { return d.value })
+		 //    })
+		    console.log('pudding')
+		    // console.log(min)
+		    // console.log(max)
+
+		    var y = d3.scale.linear()
+		    	.rangeRound([height, 0])
+
+	    	x.domain(data.map(function (d) { return d.quarter; }));
+	        y.domain([
+	          d3.min(seriesData, function (c) { 
+	            return d3.min(c.values, function (d) { return d.value; });
+	          }),
+	          d3.max(seriesData, function (c) { 
+	            return d3.max(c.values, function (d) { return d.value; });
+	          })
+	        ]);
+
+			// var yScale = d3.scale.linear()
+			// 	.domain([min, max / 1000])
+			// 	.range([0, height])
+		// }
+		// setScales(seriesData)
+
+		var line = d3.svg.line()
+          .interpolate("cardinal")
+          .x(function (d) { return x(d.label) + x.rangeBand() / 2; })
+          .y(function (d) { return y(d.value); })
+
+		var series = svg.selectAll(".series")
+		    .data(seriesData)
+		  .enter().append("g")
+		    .attr("class", "series");
+
+		series.append("path")
+		  .attr("class", "line")
+		  .attr("d", function (d) { console.log(d.values); return line(d.values); })
+		  .style("stroke", function (d) { return color(d.name); })
+		  .style("stroke-width", "4px")
+		  .style("fill", "none");
+
+
+
+		// //positions the lines
+		// var setLines = function(line) {
+		// 	line.attr("d", function (d) { return d.values })
+		// 		.attr("stroke", '#008080')
+		// 		.attr("stroke-width", "4px")
+		// 		.attr("fill", "none");
+		// }
+
+		// //draws the lines on the chart
+		// var draw = function(seriesData) {
+		// 	setScales(seriesData)
+
+		// 	var lines = g.selectAll('path')
+		// 		.data(seriesData)
+		// 		.enter()
+		// 		.append('path')
+		// 		.call(setLines)
+		// }
+		// draw(seriesData)
+})
--- main.js
+++ main.js
@@ -42,6 +42,101 @@ uvpApp.controller('SupportController', function($scope) {
 
 })
 
+//controller for d3 visualization
+uvpApp.controller('d3', function($scope) {
+	$scope.data = data
+})
+
+//'at-a-glance' directive for d3 chart
+.directive('at-a-glance', function($filter, $scope) {
+	return {
+		restrict:'E',
+
+		//define scope variables
+		scope:{
+			data:'='
+		},
+
+		link:function(scope,elem,attrs){
+			//wrapper element the chart will go in
+			wrapper = d3.select(elem[0])
+
+			var xScale;
+			var yScale;
+			var svg = wrapper.append('svg')
+				.attr('height', 400)
+				.attr('width', 400)
+
+				//margin
+				var margin = {
+					left: 70,
+					bottom: 100,
+					top:50,
+					right:50
+				}
+
+				//chart height and width
+				var height = 400 - margin.bottom - margin.top
+				var width = 400 - margin-left - margin.right
+
+				//sets the scales
+				var setScales = function(){
+					//xScale
+
+					//yScale
+
+				}
+
+				//initiate scale variables
+				setScales()
+
+				//positions paths
+				var setPaths = function(path) {
+
+				}
+
+			// Define x axis
+			var xAxis = d3.svg.axis()
+						.scale(xScale)
+						.orient('bottom')
+
+			// Define y axis
+			var yAxis = d3.svg.axis()
+						.scale(yScale)
+						.orient('left')
+
+			// Append x axis
+			svg.append(svg).call(xAxis)
+				.attr('transform', 'translate(' + margin.left + ',' + (height + margin.top) + ')')
+				.attr('class', 'axis')
+			
+			svg.append(svg)
+				.attr('class', 'axis').call(yAxis)
+				.attr('transform', 'translate(' + margin.left + ',' + (margin.top) + ')')
+
+			// Write a reusable drawing function for circles
+			var draw = function() {
+				// Filter down your data based on your search
+				//scope.filteredData = $filter('filter')(scope.data, scope.search);
+				
+				// Set Scales
+				setScales()
+				
+				// Use the .enter() method to get your entering elements, and then position them using your positioning function
+				circles.enter().append('circle').call(setPaths)
+				  
+				// Use the .exit() and .remove() methods to remove elements that are no longer in the data
+				circles.exit().remove()
+			  
+				// Select all circle elements within your g and transition their position using your positioning function
+				g.selectAll('circle').transition().duration(1500).call(circleFunc)     
+			
+			}
+			draw()
+		}
+	}
+})
+
 //set the default state to 'home' so the site loads correctly
 uvpApp.run(['$state', function ($state) {
 	$state.go('home')
